d67c1a4e54049a822f8651aa284b3f84d56bcfdf
Satya Shah <88096065+satyashah@users.noreply.github.com>
2024-03-04 17:16:45 -0500

    Add files via upload

--- Bird_Monitor_v1/func.py
+++ Bird_Monitor_v1/func.py
@@ -1,34 +1,33 @@
 import cv2
 import numpy as np
 
-def get_clean_image(neg_baseline, img=None, img_path=None):
+def get_neg_baseline(neg1, neg2):
+    """
+    This function takes two negative baseline images and returns the average and std of the two.
+    """
+    neg_std = np.mean(np.array(cv2.absdiff(neg1, neg2)))
+    neg_baseline = np.mean(np.array([neg1, neg2]), axis=0).round()
+    return neg_baseline, neg_std
+
+def get_clean_image(neg_baseline, neg_std, img):
     """
     This function takes an image path and a negative baseline image of birds and returns the simple bird image.
     """
 
-    if isinstance(img_path, str) and img_path:
-        img = cv2.imread(img_path, flags=0)
-    elif img is None:
-        raise ValueError("Please provide an image path or an image")
-
-    bird_neg = cv2.imread(neg_baseline, 0)
-
-    bird_frame = cv2.absdiff(img, bird_neg)
+    print("Shape of img:", img.shape)
+    print("Shape of neg_baseline:", neg_baseline.shape)
+    bird_frame = cv2.absdiff(img, neg_baseline)
 
     resized_bird_frame = cv2.resize(bird_frame, (280, 280)) 
     
     return resized_bird_frame
 
 
-def detect_pos_line(cleaned_pos_img=None, cleaned_pos_img_path=None):
+def detect_pos_line(cleaned_pos_img):
     """
     This function takes a bird image and returns the average x coordinate of the bird.
     """
-    if cleaned_pos_img_path is not None:
-        cleaned_pos_img = cv2.imread(cleaned_pos_img_path, flags=0)
-    elif cleaned_pos_img is None:
-        raise ValueError("Please provide a cleaned image or a cleaned image path")
-
+    
     coords = np.argwhere(cleaned_pos_img == 1)
     print(coords)
 
