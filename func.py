42d55401fedce68dd352f982114ce59cb0bb16c6
Satya Shah <88096065+satyashah@users.noreply.github.com>
2024-03-04 21:50:50 -0500

    new file:   __pycache__/func.cpython-311.pyc 	modified:   func.py 	new file:   ideas.txt 	new file:   identify_bird.py 	new file:   test_images/resize_bird.jpg 	deleted:    v1.py 	renamed:    detect_movement.py -> v1/detect_movement.py 	renamed:    edge_detection.py -> v1/edge_detection.py 	renamed:    get_images.py -> v1/get_images.py 	renamed:    image_test.py -> v1/image_test.py 	renamed:    main.py -> v1/main.py 	renamed:    remove_background.py -> v1/remove_background.py

--- func.py
+++ func.py
@@ -1,26 +1,78 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 def get_neg_baseline(neg1, neg2):
     """
     This function takes two negative baseline images and returns the average and std of the two.
+
+    Inputs: Two negative baseline images
+    Outputs: The average image of the two images and the standard deviation of the two images
     """
     neg_std = np.mean(np.array(cv2.absdiff(neg1, neg2)))
-    neg_baseline = np.mean(np.array([neg1, neg2]), axis=0).round()
+    neg_baseline = np.mean(np.array([neg1, neg2]), axis=0).round().astype('uint8') # Saves as an array of ints
+    neg_baseline = cv2.cvtColor(neg_baseline, cv2.COLOR_GRAY2BGR) # Converts into an image for CV2
     return neg_baseline, neg_std
 
-def get_clean_image(neg_baseline, neg_std, img):
+def get_bird(neg_baseline, img):
     """
-    This function takes an image path and a negative baseline image of birds and returns the simple bird image.
+    This function takes the image and negative baseline image of birds and returns the bird plot and center of the bird.
     """
 
-    print("Shape of img:", img.shape)
-    print("Shape of neg_baseline:", neg_baseline.shape)
+    X_SIZE = 280
+    Y_SIZE = 280
+
     bird_frame = cv2.absdiff(img, neg_baseline)
 
-    resized_bird_frame = cv2.resize(bird_frame, (280, 280)) 
+    resized_bird_frame = cv2.resize(bird_frame, (X_SIZE, Y_SIZE)) 
+
+    # Thresholding
+    # image of interest, threshold value, what value to set too if above threshold, type of threshold
+    threshold_value = 20
+    _, filtered_bird_frame = cv2.threshold(resized_bird_frame, threshold_value, 255, cv2.THRESH_BINARY)
+    print(filtered_bird_frame)
+
+    # Now we have the filtered_bird_frame we want to make sure no values are to far from center of mass we will call these outliers
+
+    com = [X_SIZE/2, Y_SIZE/2]  # Set to center of image
+    #Get distance of all points that are =255 from center of mass Finda all distances that are outliers and sets them to 0 on filtered_bird_frame
+
+    # Get coordinates of all points that are =255
+    point_arr = np.argwhere(filtered_bird_frame == 255)
+
+    # Run k-means clustering to find the bird versus the background
+    kmeans = KMeans(n_clusters=3, random_state=0).fit(point_arr)
+    print(kmeans.labels_)
+    print(kmeans.cluster_centers_)
     
-    return resized_bird_frame
+    # Plot the k-means clustering
+    plt.scatter(point_arr[:, 1], point_arr[:, 0], c=kmeans.labels_)
+    plt.scatter(kmeans.cluster_centers_[:, 1], kmeans.cluster_centers_[:, 0], c='r')
+    plt.show()
+
+    # Keep all points that are not labeled as outliers (check which group is closest to middle)
+    # Find the group that is closest to the middle
+    center_group = np.argmin(np.linalg.norm(kmeans.cluster_centers_ - com, axis=1))
+    # print(center_group)
+
+    # Keep all points that are in the center group
+    for i, label in enumerate(kmeans.labels_):
+        if label != center_group:
+            filtered_bird_frame[point_arr[i, 0], point_arr[i, 1]] = 0
+    
+
+
+    # Find center of mass
+    com = np.argwhere(filtered_bird_frame == 255).mean(axis=0).astype(int)
+    # print(com)
+
+    # plt.imshow(img, cmap='gray')
+    # plt.scatter(com[1], com[0], c='r')
+    # plt.show()
+
+
+    return filtered_bird_frame, com
 
 
 def detect_pos_line(cleaned_pos_img):
--- v1/main.py
+++ v1/main.py
@@ -1,7 +1,5 @@
 from func import *
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
+
 
 print("Running main.py")
 
@@ -12,24 +10,27 @@ bird_right_less = cv2.imread('test_images/bird_fake_right_less.jpg', 0)
 bird_right = cv2.imread('test_images/bird_fake_right.jpg', 0)
 
 # Show neg baseline
-
-
-
-
 neg_avg, neg_std = get_neg_baseline(bird_neg1, bird_neg2)
-print(neg_avg)
 
-cv2.imshow('Neg Baseline', neg_avg)
-cv2.waitKey(0)
+# Algorithm that A) cleans out all the noise and B) finds the center of the bird
+# bird_pos_plot, pos_com = get_bird(neg_avg, bird_pos)
+bird_right_plot, right_com = get_bird(bird_neg1, bird_right)
 
-bird_pos_image = get_clean_image(bird_neg1, neg_std, bird_pos)
-bird_left_image = get_clean_image(bird_neg1, neg_std, bird_right)
+# Show the images
+plt.imshow(bird_right, cmap='gray')
+
+point_arr = np.argwhere(bird_right_plot == 255)
+plt.scatter(point_arr[:, 1], point_arr[:, 0], c = 'b')
+plt.scatter(right_com[1], right_com[0], c='r')
+plt.show()
+
 
 
 # # Show the cleaned images
-# cv2.imshow('Bird Pos', bird_pos_image)
+cv2.imshow('Bird Pos', bird_pos)
+cv2.imshow('Bird Pos Clean', bird_pos_clean)
 # cv2.imshow('Bird Left', bird_left_image)
-# cv2.waitKey(0)
+cv2.waitKey(0)
 
 
 # # Save the images
